cc6b6ce21e8b932fb8b3cf297b2b3124342a0a36
ᗪ乇ᐯ <46674147+dev7060@users.noreply.github.com>
2021-07-12 15:12:20 +0530

    Create 122193140.cpp

---
+++ 122193140.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+using namespace std;
+ 
+int main(){
+    string str;
+    getline(cin,str);
+    int alp[26];
+    for(int i=0; i<26; i++){
+        alp[i]=0;
+    }
+    //97 to 122
+    for(int i=0; i<str.length(); i++){
+        int ch = str[i];
+        alp[97-ch]++;
+    }
+    int dis = 0;
+    for(int i=0; i<26; i++){
+        if(alp[i]!=0){
+            dis++;
+        }
+    }
+    if(dis%2!=0){
+        cout<<"IGNORE HIM!";
+    }else{
+        cout<<"CHAT WITH HER!";
+    }
+    return 0;
+}
